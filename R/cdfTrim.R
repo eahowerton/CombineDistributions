2fc5aa1a20c59f6eea18a9770e26ab5f10dc933f
eahowerton <eah394@psu.edu>
2021-05-11 07:46:36 -0400

    Create cdfTrim.R and test_cdfTrim.R

---
+++ R/cdfTrim.R
@@ -0,0 +1,41 @@
+#' Implement cdf trimming - TO UPDATE
+#'
+#' Cdf trimming removes a set number of innermost/outermost probabilities.
+#'   at each value before averaging **ADD REF**.
+#'
+#' @param data data.frame containing ids, quantiles, and values
+#' @param trim_type string specifying "interior" or "exterior"
+#' @param n_trim integer specifying the number of values to trim
+#'
+#' @return data.frame with columns \code{id}, \code{quantile}, and \code{value} containing trimmed ecdfs
+#'
+#' @details \code{keep_vals} should contain integer values between 1 and \code{n_id},
+#'   the number of unique ids. Then, at each value, \code{trim_cdf()} will order probabilities and
+#'   keep only those that defined by \code{keep_vals}. For example, if \code{n_id = 5} and
+#'   \code{keep_vals = 2:4}, \code{trim_cdf()} will return the second, third and fourth ranked probability,
+#'   excluding the smallest and larges probabilities from the average.
+#'
+#'
+#' @inheritParams mean_trim
+#' @return data.frame containing \code{quantile} and \code{value} of aggregate distribution,
+#'   plus columns specifying \code{direction}, \code{method}, \code{trim_type}, and \code{n_trim}.
+#'   TO DO: MAKE THIS CONSISTENT
+#'
+weights_trim_cdf <- function(data, trim_type, n_trim){
+  n_models <- length(unique(data$id))
+  keep_vals <- keep_vals(trim_type, n_trim, n_models)$keep
+  weighted_data <- implement_trim_cdf(data, keep_vals)
+  return(weighted_data)
+}
+
+#### HELPERS ####
+
+implement_trim_cdf <- function(data, keep_vals){
+  df_cdfs <- data %>%
+    dplyr::group_by(value) %>%
+    dplyr::mutate(rank = rank(quantile, ties.method = "first"),
+                  weight = ifelse(rank %in% keep_vals, 1, 0)) %>%
+    dplyr::select(-rank)
+  return(df_cdfs)
+}
+
---
+++ tests/testthat/test_cdfTrim.R
@@ -0,0 +1,42 @@
+suppressMessages({
+  library(testthat)
+  library(dplyr)
+  library(tibble)
+})
+source_test_helpers("R/determineTrimVals.R")
+
+
+#### weights_trim_cdf() ####
+test_that("Test weights_trim_cdf()", {
+  df_long <- data.frame(id = c(rep("A",3), rep("B",3),rep("C",3)),
+                        value = c(1,2,3,1,2,3,1,2,3),
+                        quantile = c(0,0.2,0.4,0.1,0.3,0.5,0.2,0.4,0.6))
+  trim_type = "interior"
+  n_trim = 1
+  # check a set of keep values
+  expected <- tibble(id = c(rep("A",3), rep("B",3),rep("C",3)),
+                     value = c(1,2,3,1,2,3,1,2,3),
+                     quantile = c(0,0.2,0.4,0.1,0.3,0.5,0.2,0.4,0.6),
+                     weight = c(rep(1,3), rep(0,3),rep(1,3))) %>%
+    group_by(value)
+  expect_identical(weights_trim_cdf(df_long, trim_type, n_trim), expected)
+})
+
+#### trim_cdf() ####
+test_that("Test implement_trim_cdf()",{
+  df_long <- data.frame(quantile = c(0,0.5,1,0,0,0.5),
+                        value = c(1,1,1,2,2,2))
+  # check a set of keep values
+  keep = 1:2
+  expected <- df_long %>%
+    mutate(weight = c(1,1,0,1,1,0)) %>%
+    group_by(value)
+  expect_identical(implement_trim_cdf(df_long, keep), expected)
+  # check a second set of keep values
+  keep = c(1,3)
+  expected <- df_long %>%
+    mutate(weight = c(1,0,1,1,0,1)) %>%
+    group_by(value)
+  expect_identical(implement_trim_cdf(df_long, keep), expected)
+})
+
