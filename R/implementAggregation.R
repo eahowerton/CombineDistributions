5e4dd51b661d35eba5311e14bf451889a99711be
eahowerton <eah394@psu.edu>
2021-06-21 16:52:19 -0400

    Move to data.table

--- R/implementAggregation.R
+++ R/implementAggregation.R
@@ -3,6 +3,8 @@
 #' Given a data.frame containing cdfs , return a single aggregate
 #'   cdf using specified method.
 #'
+#' @import data.table
+#'
 #' @param data data.frame that contains multiple cdfs, grouped by \code{by} column.
 #'   Specify cdf with \code{quantile} and \code{value} columns,
 #' @param id_var string containing the name of the column that identifies unique cdfs
@@ -20,40 +22,29 @@
 #'   Trim inputs should be one of the following: "cdf_interior", "cdf_exterior",
 #'   "mean_interior", "mean_exterior", or "none" following REF.
 aggregate_cdfs <- function(data, id_var, group_by, method, ret_quantiles, trim = "none", n_trim = NA){
-  unq_groups <- data %>%
-    dplyr::select(group_by) %>%
-    dplyr::distinct()
-  aggs <- apply(unq_groups, 1, apply_aggregation,data = data, id_var = id_var, method = method, ret_quantiles = ret_quantiles, trim = trim, n_trim = n_trim)
-  aggs <- do.call(rbind, aggs)
-  aggs <- do.call(rbind, aggs)
+  data <- update_id_var_col(data, id_var)
+  data.table::setDT(data)
+  aggs <- data[,calculate_single_aggregate(quant = quantile, val = value, id = id,
+                                              method = "LOP", ret_quantiles = c(0.25,0.5,0.75)),
+               by = group_by]
   return(aggs)
-}
-
-apply_aggregation <- function(data, groups, id_var, method, ret_quantiles, trim = "none", n_trim = NA){
-  data = data %>% dplyr::filter(eval(parse(text = create_filter_string(groups))))
-  agg <- calculate_single_aggregate(data, id_var, method, ret_quantiles, trim, n_trim)
-  if(all(is.na(agg))){agg <- data.frame(quantile = ret_quantiles, value = NA)}
-  grps_df <- as.data.frame(matrix(rep(groups, times = nrow(agg)), nrow = nrow(agg), byrow = TRUE))
-  colnames(grps_df) <- names(groups)
-  agg <- agg %>% dplyr::bind_cols(grps_df)
-  return(list(agg))
 }
 
 #' export
 calculate_single_aggregate <- function(quant, val, id, method, ret_quantiles, trim = "none", n_trim = NA){
-#  data <- prep_input_data(data, id_var)
+  data <- prep_input_data(quant, val, id, id_var)
   method_fn <- ifelse(method == "LOP", LOP, vincent)
-#  if(nrow(data) == 0){return(NA)}
+  if(nrow(data) == 0){return(NA)}
   if(trim == "none"){
-    agg <- method_fn(quant, val, id, ret_quantiles)
+    agg <- method_fn(data, ret_quantiles)
   }
   else{
     parse <- parse_trim_input(trim)
     if(parse[1] == "cdf"){
-      agg <- method_fn(quant, val, id, ret_quantiles, weight_fn = cdf_trim, trim_type = parse[2], n_trim, avg_dir = method)
+      agg <- method_fn(data, ret_quantiles, weight_fn = cdf_trim, trim_type = parse[2], n_trim, avg_dir = method)
     }
     else{
-      agg <- method_fn(quant, val, id, ret_quantiles, weight_fn = mean_trim, trim_type = parse[2], n_trim)
+      agg <- method_fn(data, ret_quantiles, weight_fn = mean_trim, trim_type = parse[2], n_trim)
     }
   }
   return(data.frame(quantile = ret_quantiles, value = agg))
@@ -61,33 +52,21 @@ calculate_single_aggregate <- function(quant, val, id, method, ret_quantiles, tr
 
 
 #### PREP INPUT DATA ####
-prep_input_data <- function(data, id_var){
-  data <- update_id_var_col(data, id_var)
-  data <- filter_input_data(data)
-}
-
-update_id_var_col <- function(data, id_var){
-  group_index <- which(colnames(data) == id_var)
-  colnames(data)[group_index] = "id"
+prep_input_data <- function(quant, val, id, id_var){
+  data <- data.table::data.table(id = id, quantile = quant, value = val)
+  rm_na <- check_na_vals(data)
+  data <- data[!(id %in% rm_na)]
+  rm_nonmono <- check_monotonic(data)
+  data <- data[!(id %in% rm_nonmono)]
   return(data)
 }
 
-filter_input_data <- function(data){
-  data <- data %>% dplyr::filter(!(id %in% check_na_vals(data)))
-  #data <- data %>% dplyr::filter(!(id %in% check_num_unq_vals(data)))
-  data <- data %>% dplyr::filter(!(id %in% check_monotonic(data)))
-  return(data)
-}
 
 # cannot have NA values
 check_na_vals <- function(data){
-  n_na_vals <- data %>%
-    dplyr::group_by(id) %>%
-    dplyr::summarise(l = length(which(is.na(value))))
-  rm_ids <- n_na_vals %>%
-    dplyr::filter(l >= 1) %>%
-    dplyr::pull(id)
-  if(length(rm_ids) != 0){
+  na_vals <- data[is.na(value), .(id)]
+  rm_ids <- unique(na_vals)
+  if(nrow(rm_ids) != 0){
     warning(paste0('excluding id(s) ',rm_ids,': NA value'))
   }
   return(as.character(rm_ids))
@@ -96,31 +75,11 @@ check_na_vals <- function(data){
 # cdfs must be non decreasing
 check_monotonic <- function(data){
   eps <- -0.001
-  ids <- unique(data$id)
-  n_nonpos_diff <- sapply(ids, function(i){
-    newd <- data %>%
-      dplyr::filter(id == i) %>%
-      dplyr::arrange(quantile)
-    diff_vals <- diff(newd$value)
-    return(length(which(diff_vals<eps)))
-  })
-  rm_ids <- ids[which(n_nonpos_diff > 0)]
-  if(length(rm_ids) != 0){
-    warning(paste0('excluding id(s) ',rm_ids,': not a cdf'))
-  }
-  return(as.character(rm_ids))
-}
-
-# cannot have a single value for all quantiles
-check_num_unq_vals <- function(data){
-  n_unique_vals <- data %>%
-    dplyr::group_by(id) %>%
-    dplyr::summarise(l = length(unique(value)))
-  rm_ids <- n_unique_vals %>%
-    dplyr::filter(l <= 1) %>%
-    dplyr::pull(id)
-  if(length(rm_ids) != 0){
-    warning(paste0('excluding id(s) ',rm_ids,': single value for all quantiles'))
+  diff_data <- data[order(quantile),.(dif = diff(value)), by = "id"]
+  rm_ids <- diff_data[dif < eps, .(id)]
+  rm_ids <- unique(rm_ids)
+  if(nrow(rm_ids) != 0){
+    warning(paste0('excluding id(s) ',rm_ids$id,': not a cdf'))
   }
   return(as.character(rm_ids))
 }
@@ -131,12 +90,9 @@ parse_trim_input <- function(trim){
   return(split)
 }
 
-create_filter_string <- function(groups){
-  filter_string = rep(NA, length(groups))
-  for(i in 1:length(groups)){
-    filter_string[i] = paste0(names(groups[i]), "== '", groups[i],"'")
-  }
-  filter_string <- paste0(filter_string, collapse = ' & ')
-  return(filter_string)
+update_id_var_col <- function(data, id_var){
+  group_index <- which(colnames(data) == id_var)
+  colnames(data)[group_index] = "id"
+  return(data)
 }
 
