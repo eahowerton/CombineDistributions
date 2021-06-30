3e082c945418197a4540fbaedc1d9ea36d9fe9fc
eahowerton <eah394@psu.edu>
2021-06-30 11:16:08 -0400

    Update implementAggregation.R

--- R/implementAggregation.R
+++ R/implementAggregation.R
@@ -25,7 +25,7 @@ aggregate_cdfs <- function(data, id_var, group_by, method, ret_quantiles, trim =
   data <- update_id_var_col(data, id_var)
   data.table::setDT(data)
   aggs <- data[,calculate_single_aggregate(quant = quantile, val = value, id = id,
-                                              method = "LOP", ret_quantiles = ret_quantiles,
+                                              method = method, ret_quantiles = ret_quantiles,
                                            trim = trim, n_trim = n_trim),
                by = group_by]
   return(aggs)
